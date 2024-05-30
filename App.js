769a5332d4796b4326e99bed07d19c91ece457a0
asa-siphuma <asasiphumat@gmail.com>
2024-05-30 10:27:55 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,60 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 import React from "react";
-import { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import ProfileHeader from "./frontend/src/Components/ProfileHeader";
-import ProfilePage from "./frontend/src/Screens/ProfilePage";
-import { EditProfile } from "./frontend/src/Screens/EditProfile";
-import Icon from "react-native-vector-icons/MaterialIcons";
-import CustomDrawer from "./frontend/src/Components/ProfileDrawer";
+import HomePage from "./frontend/src/Screens/HomePage";
+import MainHeader from "./frontend/src/Components/MainHeader";
+import BottomHeader from "./frontend/src/Components/BottomHeader"
 
 const Nav = createNativeStackNavigator();
 
 export default function App() {
-    const [drawerVisible, setDrawerVisible] = useState(false);
+  return (
 
-    const toggleDrawer = () => {
-        setDrawerVisible(!drawerVisible);
-    };
+    <NavigationContainer>
+      <Nav.Navigator initialRouteName="HomePage">
+        <Nav.Screen
+          name="HomePage"
+          component={HomePage}
+          options={{ header: () => <MainHeader /> }}
+        />
+      </Nav.Navigator>
+      <BottomHeader/>
+      <StatusBar style="auto" />
+    </NavigationContainer>
 
-    const closeDrawer = () => {
-        setDrawerVisible(false);
-    };
-    return (
-        <NavigationContainer>
-            <Nav.Navigator initialRouteName="ProfilePage">
-                <Nav.Screen
-                    name="ProfilePage"
-                    component={ProfilePage}
-                    options={({ navigation }) => ({
-                        header: () => <ProfileHeader toggleDrawer={toggleDrawer} navigation={navigation} />,
-                    })}
-                />
-                <Nav.Screen
-                    name="EditProfile"
-                    component={EditProfile}
-                    options={() => ({
-                        title: "Edit Profile",
-                        headerStyle: {
-                            backgroundColor: "#fff",
-                        },
-                        headerTintColor: "#000",
-                        headerTitleStyle: {
-                            fontWeight: "bold",
-                        },
-                        headerRight: () => (
-                            <View style={{ marginRight: 10 }}>
-                                {/* Your menu icon component */}
-                                <Text onPress={toggleDrawer}>
-                                    <Icon name="menu" size={24} />
-                                </Text>
-                            </View> 
-                        ),
-                    })}
-                />
-            </Nav.Navigator>
-            {drawerVisible && <CustomDrawer navigation={navigation} closeDrawer={closeDrawer} />}
-        </NavigationContainer>
-    );
+  );
 }
