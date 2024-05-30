2662bc88a6fd914a54b506b546dca7f0872fdc74
CharlizeHanekom <charlize.hanekom@gmail.com>
2024-05-30 09:09:54 +0200

    Improved styling

--- frontend/src/Screens/SignupPage.js
+++ frontend/src/Screens/SignupPage.js
@@ -5,45 +5,71 @@ import facebook from '../../../assets/facebook.png';
 import twitter from '../../../assets/twitter.png';
 
 const SignupPage = () => {
-    const [text, onChangeText] = React.useState('Useless Text');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Create Account</Text>
-            <TextInput
-                style={styles.inputText}
-                onChangeText={onChangeText}
-                value=''
-                placeholder='Userrname'
-            />
-            <TextInput
-                style={styles.inputText}
-                onChangeText={onChangeText}
-                value=''
-                placeholder='Email'
-            />
-            <TextInput
-                style={styles.inputText}
-                onChangeText={onChangeText}
-                value=''
-                placeholder='Password'
-            />
-            <TextInput
-                style={styles.inputText}
-                onChangeText={onChangeText}
-                value=''
-                placeholder='Confirm Password'
-            />
+            <View>
+                <Text style={styles.label}>Username</Text>
+                <TextInput
+                    style={styles.inputText}
+                    onChangeText={setUsername}
+                    value=''
+                    placeholder='Userrname'
+                />
+            </View>
+            <View>
+                <Text style={styles.label}>Email</Text>
+                <TextInput
+                    style={styles.inputText}
+                    onChangeText={setEmail}
+                    value=''
+                    placeholder='Email'
+                />
+            </View>
+            <View>
+                <Text style={styles.label}>Password</Text>
+                <TextInput
+                    style={styles.inputText}
+                    onChangeText={setPassword}
+                    value=''
+                    placeholder='Password'
+                    secureTextEntry={true}
+                />
+            </View>
+            <View>
+                <Text style={styles.label}>Confirm Password</Text>
+                <TextInput
+                    style={styles.inputText}
+                    onChangeText={setConfirmPassword}
+                    value=''
+                    placeholder='Confirm Password'
+                    secureTextEntry={true}
+                />
+            </View>
             <Button
                 title='Sign Up'
                 color='#000000'
+                paddingHorizontal='50'
             />
+            <View style={styles.or}>
+                <View style={styles.line}/>
+                <Text style={{fontSize:17}}>Or</Text>
+                <View style={styles.line}/>
+            </View>
             <View style={styles.socialContainer}>
                 <Image style={styles.socialLink} source={google}/>
                 <Image style={styles.socialLink} source={facebook}/>
                 <Image style={styles.socialLink} source={twitter}/>
             </View>
-            <Text style={styles}>Already have an account? Sign Up</Text>
+            <View style={styles.signupLink}>
+                <Text style={{fontSize:17}}>Already have an account? </Text>
+                <Text style={styles.link}>Login</Text>
+            </View>
         </View>
     )
 };
@@ -58,7 +84,7 @@ const styles = StyleSheet.create({
     },
     inputText: {
         height: 30,
-        width: 200,
+        width: 250,
         borderColor: '#000',
         borderWidth: 1,
         paddingHorizontal: 10,
@@ -68,9 +94,11 @@ const styles = StyleSheet.create({
         borderRadius: 5,
     },  
     socialContainer: {
-        flexDirection: 'row'
+        flexDirection: 'row',
     },
-
+    socialLink: {
+        marginHorizontal: 20
+    },
     container: {
         flex: 1,
         justifyContent: 'space-between',
@@ -79,36 +107,36 @@ const styles = StyleSheet.create({
         backgroundColor: '#ffffff',
         paddingVertical: 70,
     },
-    justforyou: {
-        paddingTop: 3,
-        textAlign: 'center',
-        fontFamily: 'Roboto',
-        color: '#000000',
-        fontSize: 20,
-        fontWeight: 'bold',
-
-    },
-
-    trending :{
-        paddingLeft:10,
-        paddingTop: 20,
-        fontFamily: 'Roboto',
-        color: '#000000',
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-
-    viewall: {
+    signupLink: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingRight: 10,
-        backgroundColor: '#fffff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
     },
-
-    viewalltext: {
-        paddingTop: 25,
-        fontFamily: 'Roboto',
-    }        
+    link: {
+        fontSize:17,
+        textDecorationLine: 'underline'
+    },
+    or: {
+        flexDirection: 'row',
+    },
+    line: {
+        marginHorizontal: 10,
+        height: 1,
+        width: '25%',
+        backgroundColor: '#000',
+        marginVertical: 20
+    },
+    label: {
+        fontWeight: 'bold',
+        paddingBottom:2
+    },
+    button: {
+        marginTop: 50,
+        paddingHorizontal: 20,
+    }
 });
 
 export default SignupPage;
