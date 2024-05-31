9001333b5f5ed697a0a5ea7fd9b702f91357bf3e
Jayson du Toit <u22571532@tuks.co.za>
2024-05-31 11:17:02 +0200

    new test setup made

---
+++ backend/src/__mocks__/auth.firebase.js
@@ -0,0 +1,16 @@
+// src/__mocks__/firebase/auth.js
+const mockAuth = {
+  createUserWithEmailAndPassword: jest.fn(),
+  signInWithEmailAndPassword: jest.fn(),
+  signOut: jest.fn()
+};
+
+const getAuth = jest.fn(() => mockAuth);
+
+export {
+  getAuth,
+  mockAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+};
