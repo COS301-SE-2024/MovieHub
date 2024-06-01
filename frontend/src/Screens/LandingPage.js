082c7471f461f4d74803e7c04ab8174534495d8d
CharlizeHanekom <charlize.hanekom@gmail.com>
2024-06-01 09:47:06 +0200

    Fixed scroll and layour

--- frontend/src/Screens/LandingPage.js
+++ frontend/src/Screens/LandingPage.js
@@ -9,7 +9,30 @@ import movie5 from '../../../assets/oppenheimer_movie.jpg'
 const LandingPage = () => {
     return (
         <View style={styles.container}>
-            <Image source={landing1} style={styles.image} />
+            <ScrollView horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={styles.scrollView}>
+                <View style={styles.imgContainer}>
+                    <Image source={landing1}/>
+                    <Text style={styles.subTitle}>Welcome to MovieHub</Text>
+                    <Text style={styles.text}>Discover, review, and discuss your favourite films on the ultimate platform for movie enthusiasts.</Text>
+                </View>
+                <View style={styles.imgContainer}>
+                    <Image source={landing2}/>
+                    <Text style={styles.subTitle}>Connect With Other Movie Lovers</Text>
+                    <Text style={styles.text}>Follow friends, like reviews, and comment on discussions to build your own network.</Text>
+                </View>
+                <View style={styles.imgContainer}>
+                    <Image source={landing3}/>
+                    <Text style={styles.subTitle}>Share Your Thoughts</Text>
+                    <Text style={styles.text}>Write and submit reviews, and share your thoughts with a community of movie enthusiasts.</Text>
+                </View>
+                <View style={styles.imgContainer}>
+                    <Image source={landing4}/>
+                    <Text style={styles.subTitle}>Get Personalised Recommendations</Text>
+                    <Text style={styles.text}>Enjoy movie suggestions tailored to your unique tastes and viewing history.</Text>
+                </View>
+                </ScrollView>
             <Pressable style={styles.create}>
                 <Text style={styles.createText}>Create Account</Text>
             </Pressable>
@@ -21,6 +44,31 @@ const LandingPage = () => {
 };
 
 const styles = StyleSheet.create({
+    subTitle: {
+        fontSize: 21,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 20
+    },
+    text: {
+        fontSize: 16,
+        textAlign: 'center',
+        padding: 10,
+        width: 350
+    },
+    imgContainer: {
+        width: '200',
+        height: '200',
+        padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    scrollView: {
+        flexDirection: 'row',
+        padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
     container: {
         flex: 1,
         justifyContent: 'center',
@@ -33,9 +81,7 @@ const styles = StyleSheet.create({
         margin:50,
         padding:10,
         width: '70%',
-        height: '70%',
-        borderRadius: 10,
-        borderColor: '#000000',
+        height: '200',
     },
     create: {
         margin: 10,
