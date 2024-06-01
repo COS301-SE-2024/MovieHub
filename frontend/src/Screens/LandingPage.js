088ef4d58dd7fccfa26543c14d6db6043d19d523
CharlizeHanekom <charlize.hanekom@gmail.com>
2024-06-01 08:50:23 +0200

    Fixed button styling

--- frontend/src/Screens/LandingPage.js
+++ frontend/src/Screens/LandingPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Button, Text, View, ScrollView, Image} from 'react-native';
+import { StyleSheet, Button, Pressable, Text, View, ScrollView, Image} from 'react-native';
 import landing1 from "../../../assets/landing1.png";
 import landing2 from "../../../assets/landing2.png";
 import landing3 from "../../../assets/landing3.png";
@@ -10,14 +10,12 @@ const LandingPage = () => {
     return (
         <View style={styles.container}>
             <Image source={landing1} style={styles.image} />
-            <Button
-                title="Create Account"
-                color="#000000"
-            />
-            <Button
-                title="Login"
-                color="#ffffff"
-            />
+            <Pressable style={styles.create}>
+                <Text style={styles.createText}>Create Account</Text>
+            </Pressable>
+            <Pressable style={styles.login}>
+                <Text style={styles.loginText}>Login</Text>
+            </Pressable>
         </View>
     );
 };
@@ -28,7 +26,8 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         width: '100%',
-        backgroundColor: '#fffff'
+        backgroundColor: '#ffffff',
+        paddingVertical: 50
     },
     image: {
         margin:50,
@@ -37,6 +36,34 @@ const styles = StyleSheet.create({
         height: '70%',
         borderRadius: 10,
         borderColor: '#000000',
+    },
+    create: {
+        margin: 10,
+        width: '70%',
+        paddingVertical: 10,
+        borderRadius: 10,
+        borderColor: '#000000',
+        backgroundColor: '#000000',
+        borderWidth: 1.5
+    },
+    createText: {
+        color: '#ffffff',
+        textAlign: 'center',
+        fontSize: 16
+    },
+    login: {
+        margin: 10,
+        width: '70%',
+        paddingVertical: 10,
+        borderRadius: 10,
+        borderColor: '#000000',
+        backgroundColor: '#ffffff',
+        borderWidth: 1.5
+    },
+    loginText: {
+        color: '#000000',
+        textAlign: 'center',
+        fontSize: 16
     }
 });
 
