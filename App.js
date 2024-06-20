0a94d71a0b57274ed75f9b8d4e10390bd98dc310
Zandile Modise <zandileignz472@gmail.com>
2024-06-20 14:18:24 +0200

    Update App.js
    Fixed App rendering error

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ import TermsOfUse from "./frontend/src/Screens/TermsOfUse";
 import ChangePassword from "./frontend/src/Screens/ChangePassword";
 import MovieDescriptionPage from "./frontend/src/Screens/MovieDescriptionPage"
 import CreatePost from "./frontend/src/Screens/CreatePost";
-
+import CreateWatchlist from "./frontend/src/Screens/CreateWatchlist";
 
 const Nav = createNativeStackNavigator();
 
@@ -38,15 +38,10 @@ export default function App() {
     const [navigationState, setNavigationState] = useState(null);
 
     return (
-        <NavigationContainer>
-            <Nav.Navigator
-                initialRouteName="LandingPage"
-                screenOptions={({ navigation }) => {
-                    if (!navigationState) {
-                        setNavigationState(navigation);
-                    }
-                    return {};
-                }}>
+        <NavigationContainer ref={nav => {
+            if (nav) setNavigationState(nav)
+        }}>
+            <Nav.Navigator initialRouteName="LandingPage">
 
                 {/* <Nav.Screen name="LandingPage" component={LandingPage} options={{ headerShown: false }} /> */}
                 {/* <Nav.Screen name="SignupPage" component={SignupPage} options={{ headerShown: false }} /> */}
@@ -71,8 +66,9 @@ export default function App() {
                         headerTintColor: 'black',
                     })}
                 />
+                
 
-<Nav.Screen
+                <Nav.Screen
                     name="EditProfile"
                     component={EditProfile}
                     options={({ navigation }) => ({
@@ -130,7 +126,16 @@ export default function App() {
                 <Nav.Screen name="SocialFeatures" component={SocialFeatures} options={{ title: "Social Features" }} />
                 <Nav.Screen name="UsingMovieHub" component={UsingMovieHub} options={{ title: "Using MovieHub" }} />
                 <Nav.Screen name="FAQs" component={FAQs} />
-
+                <Nav.Screen 
+                    name="CreateWatchlist" 
+                    component={CreateWatchlist} 
+                    options={({ navigation }) => ({
+                        title: "",
+                        headerShadowVisible: false,
+                        headerBackTitleVisible: false,
+                        headerTintColor: 'black',
+                    })} 
+                />
             </Nav.Navigator>
         </NavigationContainer>
     );
