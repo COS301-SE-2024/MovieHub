0d695d838238ce5b6a96307c6d131533fb8808d2
CharlizeHanekom <charlize.hanekom@gmail.com>
2024-07-24 08:50:38 +0200

    Update log.router.js

--- backend/src/Log/log.router.js
+++ backend/src/Log/log.router.js
@@ -3,5 +3,13 @@ import logController from './log.controller';
 
 const router = express.Router();
 
+router.post('/add', logController.addLog); // adds log with movie and date
 
+router.put('/edit', logController.editLog); // can update date and description
+
+router.delete('/remove', logController.removeLog); // removes log
+
+router.get('/:uid', logController.getLogsOfUser); // returns all logs of user
+
+ 
 module.exports = router;
