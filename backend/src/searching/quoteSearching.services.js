3c76a8ff868a434b3bdb97ad14b75739809ddd2a
CdrJellyfish <u22571532@tuks.co.za>
2024-08-06 20:05:42 +0200

    put the eky in the env file

--- backend/src/searching/quoteSearching.services.js
+++ backend/src/searching/quoteSearching.services.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 
 const supabaseUrl = "https://smpxgyiogmxexcsfkkuz.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtcHhneWlvZ214ZXhjc2Zra3V6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAzNzQ0MDksImV4cCI6MjAzNTk1MDQwOX0.gnVUUMsFOHHTukiq2jJNZjtJtkDYePSBKvsswrv_r-I";
+const supabaseKey = process.env.SUPABASE_KEY_QUOTE;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // const app = express();
--- backend/src/searching/supabaseQuoteClient.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require("dotenv")
-dotenv.config()
-const { createClient } = require('@supabase/supabase-js');
-
-const supabaseUrl = "https://smpxgyiogmxexcsfkkuz.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY_QUOTE;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export { supabase };
