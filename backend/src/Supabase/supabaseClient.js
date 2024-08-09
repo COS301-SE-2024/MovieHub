21b8715d1a8b557855ed94be69b94cb9c400c69a
CharlizeHanekom <charlize.hanekom@gmail.com>
2024-08-09 08:48:44 +0200

    Code for images
    Starter code for supabase image storage

---
+++ backend/src/Supabase/supabaseClient.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://your-project-url.supabase.co';
+const supabaseKey = 'your-anon-key';
+export const supabase = createClient(supabaseUrl, supabaseKey);
