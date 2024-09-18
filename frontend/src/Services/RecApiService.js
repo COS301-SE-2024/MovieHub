6593a84f83dd97b5bd2385aa3d22eec34f7b559a
Tlhalefo <77101303+tlh26@users.noreply.github.com>
2024-09-18 10:17:37 +0200

    Update RecApiService.js

--- frontend/src/Services/RecApiService.js
+++ frontend/src/Services/RecApiService.js
@@ -5,7 +5,8 @@ const localIP = getLocalIP();
 
 export const getRecommendedMovies = async (movieId) => {
     try {
-        const response = await axios.get(`http://${localIP}:3000/recommendations/${movieId}`);
+        const response = await axios.get(`http://${localIP}:3000/recommend/${movieId}`);
+        console.log("Data: " + JSON.stringify(response.data));
         return response.data; // Ensure the backend returns an array of recommended movies
     } catch (error) {
         console.error("Error fetching recommended movies:", error);
