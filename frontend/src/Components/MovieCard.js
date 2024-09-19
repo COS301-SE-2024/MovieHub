1446923547559e2ebee4b7cfcb4c05a0b3ecfde3
melengmoshokoa <133795063+melengmoshokoa@users.noreply.github.com>
2024-09-20 00:59:12 +0200

    Changed movie card component

--- frontend/src/Components/MovieCard.js
+++ frontend/src/Components/MovieCard.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Image, TouchableOpacity, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useTheme } from "../styles/ThemeContext";
-
+import { Dimensions } from 'react-native';
+import { SCREEN_WIDTH } from "@gorhom/bottom-sheet";
 export default function MovieCard({ movieId, imageUrl, title, rating, overview, date }) {
     const { theme } = useTheme();
     const [liked, setLiked] = useState(false);
@@ -12,87 +13,77 @@ export default function MovieCard({ movieId, imageUrl, title, rating, overview, 
         navigation.navigate("MovieDescriptionPage", { movieId: movieId, imageUrl: imageUrl, title: title, rating: rating, overview: overview, date: date });
     };
 
+    
+
     const handleLikePress = () => {
         setLiked(!liked);
     };
 
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            width: 400,
-            height: 500,
-            paddingRight: 15,
-            paddingLeft: 15,
-            shadowOffset: {
-                width: 0,
-                height: 3,
-            },
-            shadowOpacity: 0.5,
-            shadowRadius: 3.84,
-            elevation: 5,
-        },
-        card: {
-            position: "relative",
-            width: "79%",
-            backgroundColor: "transparent",
-            borderRadius: 10,
-            padding: 10,
-            shadowColor: "#000",
-            height: 400,
-            shadowOffset: {
-                width: 0,
-                height: 3,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
-            elevation: 5,
-        },
-        image: {
-            width: "100%",
-            height: "100%",
-            borderRadius: 10,
-            marginBottom: 10,
-        },
-        title: {
-            paddingTop: 19,
-            fontSize: 18,
-            fontWeight: "bold",
-            textAlign: "center",
-        },
-        overlay: {
-            ...StyleSheet.absoluteFillObject,
-            backgroundColor: "rgba(0, 0, 0, 0.87)",
-            borderRadius: 10,
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "column",
-        },
-        icon: {
-            marginLeft: 5,
-            fontSize: 20,
-            paddingTop: 20,
-        },
-        text: {
-            fontSize: 16,
-            color: "white",
-            paddingTop: 5,
-        },
-        iconFirst: {
-            marginLeft: 12,
-            fontSize: 20,
-            paddingTop: 20,
-        },
-    });
-
     return (
         <View style={styles.container}>
-            <TouchableOpacity onPress={handleNewUser} activeOpacity={1} style={styles.card}>
-                <Image source={{ uri: imageUrl }} style={styles.image} />
-
-                {/* <Text style={styles.title}>{title}</Text> */}
-            </TouchableOpacity>
-        </View>
+            
+        <TouchableOpacity onPress={handleNewUser} activeOpacity={1} style={styles.card}>
+            <Image source={{ uri: imageUrl }} style={styles.image} />
+            
+            {/* <Text style={styles.title}>{title}</Text> */}
+                
+        </TouchableOpacity>
+    </View>
     );
 }
+
+const screenWidth = Dimensions.get('window').width;
+ 
+const styles = StyleSheet.create({
+
+    container:{
+        flex: 1,
+        position: 'center',
+        alignItems: 'center',
+        width: screenWidth,
+        height: 430,
+        // paddingRight: 15,
+        // paddingLeft: 15,
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+
+        shadowOpacity: 0.5,
+        shadowRadius: 3.84,
+        elevation: 5,
+        paddingTop: 10,
+        paddingBottom: 120,
+
+
+    },
+    card: {
+        position: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '76%',
+        backgroundColor: 'transparent',
+        borderRadius: 10,
+        // padding: 10,
+        shadowColor: '#000',
+        height: 400,
+        shadowOffset: {
+        width: 0,
+        height: 3,
+        },
+
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+      },
+      image: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 10,
+        marginBottom: 10,
+        position: 'center',
+        alignItems: 'center',
+    },
+
+
+}) 
