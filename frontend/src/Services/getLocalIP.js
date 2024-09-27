1a0643b722714b4c25dc0401fe12fbe1e3c20c95
Tlhalefo <77101303+tlh26@users.noreply.github.com>
2024-09-27 14:06:09 +0200

    Added getLocalIP to gitIgnore

--- frontend/src/Services/getLocalIP.js
+++ frontend/src/Services/getLocalIP.js
@@ -2,14 +2,16 @@
 export const getLocalIP = () => {
     // Replace this with the actual URL you get from Expo CLI
 
-    const expoURL = 'exp://192.168.3.87:8081';
+    // const expoURL = 'exp://192.168.3.245:8081';
 
 
-    // Extract the IP address from the URL
-    const urlMatch = expoURL.match(/exp:\/\/([^:]+):\d+/);
-    if (urlMatch && urlMatch[1]) {
-        return urlMatch[1];
-    }
-    return 'localhost'; // Fallback value
+    // // Extract the IP address from the URL
+    // const urlMatch = expoURL.match(/exp:\/\/([^:]+):\d+/);
+    // if (urlMatch && urlMatch[1]) {
+    //     return urlMatch[1];
+   // }
+    return '192.168.3.218'; // Fallback value
 };
   
+
+// https://u.expo.dev/6ef1762e-e060-4f5f-98dc-2073a1f4dd47
