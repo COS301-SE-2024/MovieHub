4bbc90cec0761beec87a0342b5b3ab8430172ba3
Tlhalefo <77101303+tlh26@users.noreply.github.com>
2024-09-28 14:18:26 +0200

    Minor Tweaks

--- frontend/Chrome Extension/js/background.js
+++ frontend/Chrome Extension/js/background.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://localhost:3000?roomId=${roomId}"); // Replace with production WebSocket server
+const socket = new WebSocket("wss:///moviehub-watchparty-extension.glitch.me?roomId=${roomId}"); // Replace with production WebSocket server
 
 socket.addEventListener("message", (event) => {
     const data = JSON.parse(event.data);
