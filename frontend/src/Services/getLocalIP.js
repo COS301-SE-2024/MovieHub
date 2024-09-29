399d6f1903bef07f20ecf85142ca98ee0540f0da
CdrJellyfish <u22571532@tuks.co.za>
2024-09-29 15:03:02 +0200

    Ip change

--- frontend/src/Services/getLocalIP.js
+++ frontend/src/Services/getLocalIP.js
@@ -1,15 +1,18 @@
 // utils/getLocalIP.js
 export const getLocalIP = () => {
-//     // Replace this with the actual URL you get from Expo CLI
+    // Replace this with the actual URL you get from Expo CLI
 
-//     const expoURL = 'exp://192.168.0.113:8081';
+    const expoURL = 'exp://192.168.0.113:8081';
 
 
-//     // Extract the IP address from the URL
-//     const urlMatch = expoURL.match(/exp:\/\/([^:]+):\d+/);
-//     if (urlMatch && urlMatch[1]) {
-//         return urlMatch[1];
-//     }
-    return '13.244.236.79'; // Fallback value
+    // Extract the IP address from the URL
+    const urlMatch = expoURL.match(/exp:\/\/([^:]+):\d+/);
+    if (urlMatch && urlMatch[1]) {
+        return urlMatch[1];
+    }
+    
+
+return 'localhost'; // Fallback value
+//return '13.244.236.79'; // Fallback value
 };
   
