6df481e280d694fbdcc0e571224babe353efc95b
CdrJellyfish <u22571532@tuks.co.za>
2024-10-15 20:10:31 +0200

    update the es

--- backend/src/searching/removeUnPop.js
+++ backend/src/searching/removeUnPop.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 // Load the JSON data
-fs.readFile('merged_output.json', 'utf8', (err, data) => {
+fs.readFile('filtered_merged_output2.json', 'utf8', (err, data) => {
   if (err) {
     console.error('Error reading file:', err);
     return;
@@ -16,7 +16,7 @@ fs.readFile('merged_output.json', 'utf8', (err, data) => {
   // Function to check if any field contains the word "spanking"
   const containsSpanking = (movieData) => {
     const fieldsToCheck = ['title', 'keywords', 'overview', 'tagline'];
-    return fieldsToCheck.some(field => movieData[field] && movieData[field].toLowerCase().includes('spanking'));
+    return fieldsToCheck.some(field => movieData[field] && movieData[field].toLowerCase().includes('Seduction'));
   };
 
   for (let i = 0; i < lines.length; i += 2) {
@@ -29,7 +29,7 @@ fs.readFile('merged_output.json', 'utf8', (err, data) => {
     const releaseYear = new Date(movieData.releaseDate).getFullYear();
 
     // Check if the movie's popularity is greater than 1.5, release year is 1960 or later, and does not contain the word "spanking"
-    if (popularity > 1.5 && releaseYear >= 1960 && !containsSpanking(movieData)) {
+    if (popularity > 5 && releaseYear >= 1960 && !containsSpanking(movieData)) {
       // Keep the movie and its index in the filtered data
       filteredData.push(JSON.stringify(indexData));
       filteredData.push(JSON.stringify(movieData));
@@ -37,7 +37,7 @@ fs.readFile('merged_output.json', 'utf8', (err, data) => {
   }
 
   // Write the filtered data back to a new file
-  fs.writeFile('filtered_merged_output.json', filteredData.join('\n'), 'utf8', (err) => {
+  fs.writeFile('filtered_merged_output2.json', filteredData.join('\n'), 'utf8', (err) => {
     if (err) {
       console.error('Error writing file:', err);
       return;
